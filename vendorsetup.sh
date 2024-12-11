709371ca330249030a796914ef146615b2c3e303
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2024-12-11 17:34:29 +0530

    RMX1901: vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Colors
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+END="\033[0m"
+
+# Function to check if a directory exists
+check_dir() {
+    if [ -d "$1" ]; then
+        echo -e "${YELLOW}• $1 already exists. Skipping cloning...${END}"
+        return 1
+    fi
+    return 0
+}
+
+# Start
+echo -e "${YELLOW}Applying patches and cloning device source...${END}"
+
+
+# Clone Vendor Sources
+if check_dir vendor/realme/RMX1901; then
+    git clone --depth=1 -b 15 https://gitlab.com/shravansayz/vendor_realme_RMX1901_15.git vendor/realme/RMX1901
+fi
+
+# Clone Kernel Sources
+    git clone --depth=1 -b testtt https://github.com/shravansayz/kernel_realme_sdm710.git kernel/realme/sdm710
+    cd kernel/realme/RMX1901
+    curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
+    cd ../../..
+
+# End
+echo -e "${YELLOW}All patches have been successfully applied; your device sources are now ready!${END}"
